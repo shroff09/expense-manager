319e6ad262d94dd528a90b4a353845da72bc0f93
shroff09 <ankitnidhishroff9@gmail.com>
2020-10-01 20:57:12 +0530

    feature added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import './App.css';
 import Records from './components/Records';
- import SignUp from './components/signUp';
+ import SignUp from './components/SignUp';
  import SignIn from './components/SignIn';
  import  Heading from './components/Heading';
 import './components/style.css';
@@ -15,9 +15,8 @@ function App() {
     <Heading/>
        <Switch>
        <Route path="/home" exact component={Records}/>
+       <Route path="/signin" component={SignIn} />
       <Route path="/"  component={SignUp}/>
-      <Route path="/signin" component={SignIn} />
-      
        </Switch>
     </div>
     </Router>
